1286e022da75225dcd23601d481d4a291043d45f
Alex Swedenburg <al91dak@gmail.com>
2013-04-08 19:03:50 -0600

    Actually saved this time.

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+#include <pthread.h>
+using namespace std;
+
+namespace config {
+	string mycall;
+	int myssid;
+}
+
+int main(int argc, char* argv[]) {
+	string configfile = "/etc/aprstoolkit.conf";
+	
+	
+}
