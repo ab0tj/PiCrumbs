2d6f00e2a92cffe44e00bb82bf26cd8d88a42cf2
Alex Swedenburg <al91dak@gmail.com>
2013-04-17 14:47:56 -0600

    Some work on serial port stuf

--- main.cpp
+++ main.cpp
@@ -11,12 +11,78 @@
 
 using namespace std;
 
+// GLOBAL VARS GO HERE
 const char* version = "0.1";
 string mycall;
 bool verbose = false;
 int kiss_iface;
+int gps_iface;
 
-void init(int argc, char* argv[]) {
+int get_baud(int baudint) {		// return a baudrate code from the baudrate int
+	switch (baudint) {
+	case 0:
+		return B0;
+	case 50:
+		return B50;
+	case 75:
+		return B75;
+	case 100:
+		return B110;
+	case 134:
+		return B134;
+	case 150:
+		return B150;
+	case 200:
+		return B200;
+	case 300:
+		return B300;
+	case 600:
+		return B600;
+	case 1200:
+		return B1200;
+	case 1800:
+		return B1800;
+	case 2400:
+		return B2400;
+	case 4800:
+		return B4800;
+	case 9600:
+		return B9600;
+	case 19200:
+		return B19200;
+	case 38400:
+		return B38400;
+	case 57600:
+		return B57600;
+	case 115200:
+		return B115200;
+	case 230400:
+		return B230400;
+	default:
+		return -1;
+	}
+}	// END OF 'get_baud'
+
+int open_port(string port, int baud_code) {					// open a serial port
+	struct termios options;
+	int iface = open(port.c_str(), O_RDWR | O_NOCTTY | O_NDELAY);	// open the port
+	if (iface == -1) return -1;								// failed to open port
+	fcntl(iface, F_SETFL, 0);								// set port to blocking reads
+	tcgetattr(iface, &options);								// get current control options for the port
+	cfsetispeed(&options, baud_code);						// set in baud rate
+	cfsetospeed(&options, baud_code);						// set out baud rate
+	options.c_cflag &= ~PARENB;								// no parity
+	options.c_cflag &= ~CSTOPB;								// 1 stop bit
+	options.c_cflag &= ~CSIZE;
+	options.c_cflag |= CS8;									// 8 bit data
+	options.c_cflag |= (CLOCAL | CREAD);					// enable the receiver and set local mode
+	options.c_lflag = ICANON;								// canonical mode
+	options.c_oflag &= ~OPOST;								// raw output
+	tcsetattr(iface, TCSANOW, &options);					// set the new options for the port
+	return iface;											// return the file number
+}	// END OF 'open_port'
+
+void init(int argc, char* argv[]) {		// read config, set up serial ports, etc
 	string configfile = "/etc/aprstoolkit.conf";
 
 // COMMAND LINE ARGUMENT PARSING
@@ -54,20 +120,51 @@ void init(int argc, char* argv[]) {
 	if (verbose) printf("Using config file %s\n", configfile.c_str());
 
 	mycall = readconfig.Get("station", "mycall", "N0CALL");
-	char* kissport = readconfig.Get("tnc", "port", "/dev/ttyS0");
+	bool gps_enable = readconfig.GetBoolean("gps", "enable", false);
+	string kiss_port = readconfig.Get("tnc", "port", "/dev/ttyS0");
+	int kiss_baud = readconfig.GetInteger("tnc", "baud", 9600);
+	string gps_port  = readconfig.Get("gps", "port", "/dev/ttyS1");
+	int gps_baud = readconfig.GetInteger("gps", "baud", 4800);
 
 // OPEN KISS INTERFACE
 
-	kiss_iface = open(kissport, 0_RDWR | 0_NOCTTY | 0_NDELAY);
+	// no 'if' here, since this would be pointless without a TNC
 
-	if (kiss_iface == -1) {		// couldn't open the serial port...
-		fprintf(stderr, "Could not open KISS port %s\n", kissport);
+	int baud_code = get_baud(kiss_baud);
+
+	if (baud_code == -1) {		// get_baud says that's an invalid baud rate
+		fprintf(stderr, "Invalid KISS baud rate %i\n", kiss_baud);
 		exit (EXIT_FAILURE);
 	}
 
-	fcntl(kiss_iface, F_SETFL, 0);		// set file flags
+	kiss_iface = open_port(kiss_port, baud_code);
 
-	if (verbose) printf("Successfully opened KISS port %s\n", kissport);
+	if (kiss_iface == -1) {		// couldn't open the serial port...
+		fprintf(stderr, "Could not open KISS port %s\n", kiss_port.c_str());
+		exit (EXIT_FAILURE);
+	}
+
+	if (verbose) printf("Successfully opened KISS port %s\n", kiss_port.c_str());
+
+// OPEN GPS INTERFACE
+
+	if (gps_enable) {
+		baud_code = get_baud(gps_baud);
+
+		if (baud_code == -1) {
+			fprintf(stderr, "Invalid GPS baud rate %i\n", gps_baud);
+			exit (EXIT_FAILURE);
+		}
+
+		gps_iface = open_port(gps_port, baud_code);
+
+		if (gps_iface == -1) {
+			fprintf(stderr, "Could not open GPS port %s\n", gps_port.c_str());
+			exit (EXIT_FAILURE);
+		}
+
+		if (verbose) printf("Successfully opened GPS port %s\n", gps_port.c_str());
+	}
 
 }	// END OF 'init'
 
