41a5a7bc396e10c27cabeab66c955f165361980e
David Dee <dsdee@dsba.net>
2018-09-25 17:32:53 -0600

    Update version number

--- version.h
+++ version.h
@@ -1,4 +1,4 @@
 #ifndef VERSION
-#define VERSION "0.2"				// program version for messages, etc
+#define VERSION "0.3"				// program version for messages, etc
 #define PACKET_DEST "APMG02"		// packet tocall
 #endif
