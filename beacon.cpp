be8f664fff2f1e25e8b98f057d524e4215bef9f6
Alex Swedenburg <github@delorted.com>
2019-07-19 09:54:27 -0600

    Can't math today

--- beacon.cpp
+++ beacon.cpp
@@ -81,7 +81,7 @@ namespace beacon
 	float fround(float f, uint digits)
 	{
 		if (digits == 0) return round(f);
-		return float(int(f * (10 * digits) + 0.5)) / (digits * 10);
+		return float(int(f * pow(10, digits) + 0.5)) / pow(10, digits);
 	}
 
 	string parseComment(string text)
